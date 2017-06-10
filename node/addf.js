7cbd621c62d7728f90e7fd830d86ac2612a6ea51
dream9 <457731724@qq.com>
2017-06-10 18:30:08 +0800

    修改了样式、背景

--- node/addf.js
+++ node/addf.js
@@ -19,7 +19,7 @@ var day = date.getDate();
 var time = year + "-" + month +"-"+ day;
 
 userModSql_Params.push(time);
-userModSql_Params.push('');
+userModSql_Params.push("欢迎使用在线文档 \n\n在线文档，一份在线编辑、实时保存的文档，还可以轻松邀请好友来一起协作编辑。\n\n以下是一些在线文档使用小技巧：\n\n1、在线编辑，实时保存\n在线编辑，告别繁重的office软件；实时保存，不用担心编辑丢失。\n\n2、将文档分享给好友\n点击分享，将文档链接发送给好友。\n\n3、与好友共同编辑 \n进入文档的好友，可以与你一起编辑文档。\n\n至此，在线文档使用技巧已get。");
 userModSql_Params.push(file.by);
 
     connection.query( 
